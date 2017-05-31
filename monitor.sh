650a39b39131d19300eb48e6545df102fb446547
John Chilton <jmchilton@gmail.com>
2017-05-31 08:55:36 -0400

    Improvements to monitor.sh - allow multiple repositories.

--- monitor.sh
+++ monitor.sh
@@ -1,5 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+
+declare -a REPOSITORIES=(
+    "https://github.com/galaxyproject/tools-iuc"
+    "https://github.com/galaxyproject/tools-devteam"
+)
 
 PLANEMO_OPTIONS="--verbose"
-git clone https://github.com/galaxyproject/tools-iuc
-planemo $PLANEMO_OPTIONS container_register tools-iuc
+
+for repository in "${REPOSITORIES[@]}"
+do
+   repo_dir=`basename "$repository"`
+   git clone "$repository" "$repo_dir"
+   planemo $PLANEMO_OPTIONS container_register --recursive "$repo_dir"
+   rm -rf "$repo_dir"
+done
