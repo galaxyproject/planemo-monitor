2d329eeb45eba885ecce3c5aebaa1bb020fa6e59
John Chilton <jmchilton@gmail.com>
2017-05-31 15:43:38 -0400

    Preseed monitor script with more repositories.

--- monitor.sh
+++ monitor.sh
@@ -3,6 +3,13 @@
 declare -a REPOSITORIES=(
     "https://github.com/galaxyproject/tools-iuc"
     "https://github.com/galaxyproject/tools-devteam"
+    "https://github.com/galaxyproteomics/tools-galaxyp"
+    "https://github.com/bgruening/galaxytools"
+    "https://github.com/peterjc/pico_galaxy"
+    "https://github.com/genouest/tools-colibread"
+    "https://github.com/TGAC/earlham-galaxytools"
+    "https://github.com/AAFC-MBB/Galaxy"
+    "https://github.com/phac-nml/galaxy_tools"
 )
 
 : ${PLANEMO_TARGET:="https://github.com/jmchilton/planemo/archive/mulled_register.zip"}
