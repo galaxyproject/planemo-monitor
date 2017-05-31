d41be01fcbe4b317fc009c9b8d7d12df0df4b69c
John Chilton <jmchilton@gmail.com>
2017-05-31 09:28:36 -0400

    Move more of testing into montior.sh so this is portable to Jenkins for instance.

--- monitor.sh
+++ monitor.sh
@@ -5,7 +5,18 @@ declare -a REPOSITORIES=(
     "https://github.com/galaxyproject/tools-devteam"
 )
 
-PLANEMO_OPTIONS="--verbose"
+: ${PLANEMO_TARGET:="https://github.com/jmchilton/planemo/archive/mulled_register.zip"}
+: ${PLANEMO_OPTIONS:="--verbose"}
+
+if [ -f .venv ]; then
+    virtualenv .venv
+    . .venv/bin/activate
+    pip install -U pip
+    pip install PyGithub
+    pip install "${PLANEMO_TARGET}"
+fi
+
+planemo $PLANEMO_OPTIONS conda_init
 
 for repository in "${REPOSITORIES[@]}"
 do
