79c462a8ced6f75ab69978d36c5abb09b17a8791
mvdbeek <m.vandenbeek@gmail.com>
2021-01-26 10:56:51 +0100

    Clone repo to build at depth=1, use ubuntu 20.04
    Actual fixes are in https://github.com/galaxyproject/planemo/pull/1135

--- monitor.sh
+++ monitor.sh
@@ -16,7 +16,7 @@ planemo conda_init
 sort -R $1 | while read repository
 do
    repo_dir=$(basename "$repository")
-   git clone "$repository" "$repo_dir"
+   git clone --depth=1 "$repository" "$repo_dir"
    planemo container_register --force_push --recursive "$repo_dir"
    rm -rf "$repo_dir"
 done
