1b03c04cf7d8cce78a9b3aa4a0956720c17dd7ee
Matthias Bernt <m.bernt@ufz.de>
2023-07-26 18:29:46 +0200

    try to record exit code

--- monitor.sh
+++ monitor.sh
@@ -10,17 +10,20 @@ fi
 
 planemo conda_init
 
-sort -R $1 | while read repository
+exit_code="0"
+sort -R "$1" | while read repository
 do
     repo_dir=$(basename "$repository")
     git clone --depth=1 "$repository" "$repo_dir"
     cd "$repo_dir"
     planemo ci_find_repos  --exclude packages --exclude deprecated --exclude_from .tt_skip --output repository_list.txt
-    cat repository_list.txt | while read tool_dir
+    while read tool_dir
     do
         planemo container_register --force_push --recursive "$tool_dir"
-    done
+        exit_code=$(( "$exit_code" | "$?" ))
+    done < repository_list.txt
     cd -
     rm -rf "$repo_dir"
 done
 
+exit $exit_code
